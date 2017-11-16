4f4aa32ee1745bb10cfad009f060e82ff03798f1
Raja Sekar <rajasekar89be@gmail.com>
2017-11-16 18:33:07 +0530

    babel gulp demo

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+var x = function x(n) {
+  return n * n;
+};
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+
+gulp.task('default', () =>
+  gulp.src('./index.js')
+  .pipe(babel({ presets: ['@babel/env']} ))
+  .pipe(gulp.dest('dist'))
+);
